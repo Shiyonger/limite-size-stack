a22455c4432990eb03a17265157f0548a9317e7f
Shiyonger <alexander.gradov81@gmail.com>
2024-04-19 22:10:34 +0300

    first commit

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class StackItem<T>
+{
+	public T Val { get; set; }
+	public StackItem<T> Next { get; set; }
+	public StackItem<T> Prev { get; set; }
+}
+
+public class LimitedSizeStack<T>
+{
+	private StackItem<T> head;
+	private StackItem<T> tail;
+	private int limit;
+	private int count;
+	
+	public LimitedSizeStack(int undoLimit)
+	{
+		limit = undoLimit;
+	}
+
+	public void Push(T item)
+	{
+		if (head == null && limit != 0)
+		{
+			tail = head = new StackItem<T>() { Val = item, Next = null, Prev = null };
+			count++;
+		}
+		else if (limit != 0)
+		{
+			var temp = new StackItem<T>() { Val = item, Next = null, Prev = tail };
+			tail.Next = temp;
+			tail = temp;
+			count++;
+			if (count > limit)
+			{
+				(head.Next).Prev = null;
+				head = head.Next;
+				count--;
+			}
+		}
+	}
+
+	public T Pop()
+	{
+		T temp = tail.Val;
+		if (tail.Prev == null)
+		{
+			head = tail = null;
+			count--;
+			return temp;
+		}
+		(tail.Prev).Next = null;
+		tail = tail.Prev;
+		count--;
+		return temp;	
+	}
+
+	public int Count { get { return count; } }
+}
---
+++ ListModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+interface ICommand
+{
+	void Execute();
+	void Undo();
+}
+
+class AddItem<TItem> : ICommand
+{
+	private TItem item;
+	private int index;
+	private List<TItem> list;
+
+	public AddItem(TItem item, int index, List<TItem> list)
+	{
+		this.item = item;
+		this.index = index;
+		this.list = list;
+	}
+	
+	public void Execute()
+	{
+		list.Add(item);
+	}
+
+	public void Undo()
+	{
+		list.RemoveAt(index);
+	}
+}
+
+class RemoveItem<TItem> : ICommand
+{
+	private TItem item;
+	private int index;
+	private List<TItem> list;
+
+	public RemoveItem(TItem item, int index, List<TItem> list)
+	{
+		this.item = item;
+		this.index = index;
+		this.list = list;
+	}
+	public void Execute()
+	{
+		list.RemoveAt(index);
+	}
+
+	public void Undo()
+	{
+		list.Insert(index, item);
+	}
+}
+
+public class ListModel<TItem>
+{
+	public List<TItem> Items { get; }
+	public int UndoLimit;
+	private LimitedSizeStack<ICommand> stack; 
+        
+	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+	{
+	}
+
+	public ListModel(List<TItem> items, int undoLimit)
+	{
+		Items = items;
+		UndoLimit = undoLimit;
+		stack = new LimitedSizeStack<ICommand>(undoLimit);
+	}
+
+	public void AddItem(TItem item)
+	{
+		AddItem<TItem> cmd = new AddItem<TItem>(item, Items.Count, Items);
+		cmd.Execute();
+		stack.Push(cmd);
+	}
+
+	public void RemoveItem(int index)
+	{
+		RemoveItem<TItem> cmd = new RemoveItem<TItem>(Items[index], index, Items);
+		cmd.Execute();
+		stack.Push(cmd);
+	}
+
+	public bool CanUndo()
+	{
+		return stack.Count!=0;
+	}
+
+	public void Undo()
+	{
+		ICommand cmd = stack.Pop();
+		cmd.Undo();
+	}
+}
